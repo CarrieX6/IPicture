cdc4e221f0554c111ed1719fa57fc65c6b622cef
CarrieX6 <38813464+CarrieX6@users.noreply.github.com>
2019-07-27 20:32:24 +0800

    Add files via upload

---
+++ App_Code/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Member 的摘要说明
+/// </summary>
+public class Member
+{
+	public Member()
+	{
+		//
+		// TODO: 在此处添加构造函数逻辑
+		//
+	}
+    public string id { get; set; }
+    public string loginname { get; set; }
+    public string pwd { get; set; }
+    public string truename { get; set; }
+    public string face { get; set; }
+}
